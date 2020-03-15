4e6258ce2a77821e8126be8d8434dd995503495c
Khabz <khabubundivhu@gmail.com>
2020-03-15 20:22:29 +0200

    :tada:

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MetroRail msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MetroRail from './components/MetroRail.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MetroRail
   }
 }
 </script>
