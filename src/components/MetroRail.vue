4bd71c22c6cbb0a683523f50ed391fab62251b96
Khabz <khabubundivhu@gmail.com>
2020-03-16 01:23:36 +0200

    :sparkles: List all routes

--- src/components/MetroRail.vue
+++ src/components/MetroRail.vue
@@ -1,37 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center text-info">WC Metrorail Routes</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6 train-list">
+        <div class="card" style="width: 18rem;">
+          <ul class="list-group list-group-flush" v-for="route in routes" v-bind:key="route.id">
+            <li class="list-group-item">{{route.longName}}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-6">
+        <h2 class="text-center">Right</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
-  name: 'MetroRail',
-  props: {
-    msg: String
-  }
-}
+  name: "MetroRail",
+  data() {
+    return {
+      routes: []
+    }
+  },
+  mounted() {
+    axios.get('https://proserver.gometro.co.za/api/v1/rail/routes')
+        .then(response => {
+          this.routes = response.data
+        })
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  .train-list {
+    overflow-y: scroll;
+    height: 95vh;
+    li {
+      &:hover {
+        background-color: darkgrey;
+      }
+    }
+  }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    devServer: {
+        proxy: 'https://proserver.gometro.co.za/',
+    }
+}
