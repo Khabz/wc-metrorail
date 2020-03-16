6520b55f290019eb0b6bb59a036647087bc3ffae
Khabz <khabubundivhu@gmail.com>
2020-03-16 07:15:15 +0200

    :sparkles: Get route stops

--- src/components/MetroRail.vue
+++ src/components/MetroRail.vue
@@ -6,49 +6,75 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-6 train-list">
-        <div class="card" style="width: 18rem;">
-          <ul class="list-group list-group-flush" v-for="route in routes" v-bind:key="route.id">
-            <li class="list-group-item">{{route.longName}}</li>
+      <div class="col-3">
+        <div class="card train-list">
+          <h4 class="card-header text-info">Metrorail Routes</h4>
+          <ul class="list-group list-group-flush">
+            <li
+              class="list-group-item"
+              v-for="(route, index) in routes"
+              v-bind:key="index"
+              @click="selectRoute(route.id)"
+            >
+              {{ route.longName }}
+            </li>
           </ul>
         </div>
       </div>
-      <div class="col-6">
-        <h2 class="text-center">Right</h2>
+      <div class="col-9">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="text-info">Routes Stops</h4>
+          </div>
+          <RouteStops class="card-body" :route_stops="this.route_stops" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
+import RouteStops from './RouteStops'
 
 export default {
   name: "MetroRail",
   data() {
     return {
-      routes: []
+      routes: [],
+      route_stops: []
+    };
+  },
+  components: {
+    RouteStops
+  },
+  methods:{
+    selectRoute(val) {
+      axios.get('https://proserver.gometro.co.za/api/v1/rail/routes/'+ val +'/stops')
+          .then(response => {
+            this.route_stops = response.data
+          })
     }
   },
   mounted() {
-    axios.get('https://proserver.gometro.co.za/api/v1/rail/routes')
-        .then(response => {
-          this.routes = response.data
-        })
-  },
+    axios
+      .get("https://proserver.gometro.co.za/api/v1/rail/routes")
+      .then(response => {
+        this.routes = response.data;
+      });
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .train-list {
-    overflow-y: scroll;
-    height: 95vh;
-    li {
-      &:hover {
-        background-color: darkgrey;
-      }
+.train-list {
+  overflow-y: scroll;
+  height: 95vh;
+  li {
+    &:hover {
+      background-color: darkgrey;
     }
   }
+}
 </style>
---
+++ src/components/RouteStops.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="row map">
+        <l-map :zoom="zoom" :center="center" style="height: 95vh">
+            <l-tile-layer :url="url" 
+             :attribution="attribution"></l-tile-layer>
+            <l-marker 
+                v-for="(route_stop, index) in route_stops" :key="index"
+                :lat-lng="latLng(route_stop.lat, route_stop.lon)">
+            </l-marker>
+        </l-map>
+    </div>
+</template>
+
+<script>
+
+import L from 'leaflet';
+//import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+    name: 'RouteStops',
+    props: {
+        route_stops: Array
+    },
+    data() {
+        return {
+            zoom: 13,
+            center: L.latLng(-34.0000, 20.0000),
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            url: 'https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=7190c84edff8484ca26ba9d06421b0ea',
+            marker: L.latLng(-34.0000, 20.0000)
+        }
+    },
+    methods: {
+        latLng: function(lat, lng) {
+            return L.latLng(lat, lng)
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .map {
+        height: 95vh
+    }
+<style>
